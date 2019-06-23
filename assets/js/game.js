f3085d59d84f9d9e1fb67f5445305d91b0650391
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-22 20:45:04 -0400

    established initial variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
- 
+ //Variables
+
+ let wins = 0;
+ let losses = 0;
+ let guessesLeft = 12;
+ let wordChoices = ["dysentery", "crossing", "cholera", "oxen", "yoke", "hunt", "wagon", "landmark", "typhoid", "measles"];
+ let wordPicked = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+ let incorrectGuesses = [];
+ let lettersToGuess = wordPicked.length;
+ let displayToUser = [];
