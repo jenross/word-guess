0c165499634c7e2d92ea88da330707c5347ac0fc
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-24 15:40:46 -0400

    added reset css; fixed bug and updated lettercounter

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,17 +6,20 @@ let guessesLeft = 12;
 let wordChoices = ["dysentery", "crossing", "cholera", "oxen", "yoke", "hunt", "wagon", "landmark", "typhoid", "measles"];
 let wordPicked = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 let incorrectGuesses = [];
-let lettersToGuess = [];
 let displayToUser = [];
 let letterCounter = 0;
 
+document.getElementById("directions-text").innerHTML = "Guess the word to make it to the crossing on time!"
 //Functions
 
-//sets up & clears guesses, displays for hidden word(_)
+//sets up & clears guesses, letter counter, displays for hidden word(_); displays all game stats 
 //can use to get started and reset
 function initialize () {
+    letterCounter = 0;
     guessesLeft = 12;
+    document.getElementById("guesses-left-text").innerHTML = "Guesses remaining: " + guessesLeft;
     incorrectGuesses = [];
+    document.getElementById("incorrect-guesses-text").innerHTML = "You have already guessed: " + "";
     displayToUser = [];
     wordPicked = wordChoices[Math.floor(Math.random() * wordChoices.length)];
     //loop through wordpicked and make letters display as _
@@ -25,25 +28,25 @@ function initialize () {
     }
     console.log("Word looks like: " + displayToUser.join(" "));
     document.getElementById("word-text").innerHTML = displayToUser.join(" ");
-    document.getElementById("directions-text").innerHTML = "Guess the word to make it to the crossing on time!"
+   
+
     document.getElementById("wins-text").innerHTML = "Wins: " + wins;
     document.getElementById("losses-text").innerHTML = "Losses: " + losses;
-    document.getElementById("guesses-left-text").innerHTML = "Guesses remaining: " + guessesLeft;
-    document.getElementById("incorrect-guesses-text").innerHTML = "You have already guessed: " + "";
-    document.getElementById("notification").innerHTML = "";
 }
 
 function didYouWin () {
     //converts what user has guessed to the word to see if all letters have been guessed
     //wins, losses, resets accordingly 
-    if (letterCounter === wordPicked.length && guessesLeft > 0) {
+    if (letterCounter === wordPicked.length && guessesLeft >= 0) {
         wins++;
         document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+        alert("You have made it to the next crossing!");
         initialize();
     } else {
         if (guessesLeft === 0) {
             losses++;
             document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+            alert("You have died of dysentery.");
             initialize();
         }
     }
@@ -58,7 +61,8 @@ document.onkeyup = function(event) {
     
     //if/else to determine if user guess is part of the word
     //guess gets displayed in correct place on word if right
-    //guess gets put in incorrect guesses if wrong 
+    //guess gets put in incorrect guesses if wrong
+    //didYouWin function called to determine if entire word was guessed in time; this function also initializes new round  
     if (wordPicked.indexOf(userGuess) > -1) {
         for (let i = 0; i < wordPicked.length; i++) {
             if (wordPicked[i] === userGuess) {
@@ -66,6 +70,7 @@ document.onkeyup = function(event) {
                 console.log(displayToUser);
                 document.getElementById("word-text").innerHTML = displayToUser.join(" ");
                 letterCounter++;
+                console.log("letters counted: " + letterCounter);
                 guessesLeft--;
                 document.getElementById("guesses-left-text").innerHTML = "Guesses remaining: " + guessesLeft;
                 didYouWin();
