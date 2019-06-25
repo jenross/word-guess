e9f80ef1172dcab9ecb9514069271ed9ab2e179d
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-25 14:56:37 -0400

    added more word choices

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@
 let wins = 0;
 let losses = 0;
 let guessesLeft = 12;
-let wordChoices = ["dysentery", "crossing", "cholera", "oxen", "yoke", "hunt", "wagon", "landmark", "typhoid", "measles"];
+let wordChoices = ["dysentery", "crossing", "cholera", "oxen", "yoke", "hunt", "wagon", "landmark", "typhoid", "measles", "trail", "oregon", "river"];
 let wordPicked = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 let incorrectGuesses = [];
 let displayToUser = [];
